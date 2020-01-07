c4621a3636c3362a77cabe89f19071dca32a2bcc
ryw <yongwei_ren@baofu.com>
2020-01-07 18:03:47 +0800

    二叉树

--- src/com/test/sqlgendao/SqlGenDaoUpdate.java
+++ src/com/test/sqlgendao/SqlGenDaoUpdate.java
@@ -11,20 +11,19 @@ public class SqlGenDaoUpdate {
 	private final static boolean isAllString = false;
 
 
-	private final static String sqlString = "  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',\n"
-			+ "  `member_id` bigint(20) NOT NULL COMMENT '商户ID',\n"
-			+ "  `company_code` varchar(30) NOT NULL COMMENT '企业编码',\n"
-			+ "  `card_no` varchar(128) NOT NULL COMMENT '银行卡号',\n"
-			+ "  `card_name` varchar(128) NOT NULL COMMENT '银行卡名称',\n"
-			+ "  `id_card_no` varchar(128) DEFAULT '' COMMENT '身份证',\n"
-			+ "  `mobile` varchar(128) DEFAULT '' COMMENT '手机号',\n"
-			+ "  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '1启用 0禁用',\n"
-			+ "  `rem1` varchar(200) DEFAULT '' COMMENT '预留字段1',\n"
-			+ "  `rem2` varchar(200) DEFAULT '' COMMENT '预留字段2',\n"
-			+ "  `rem3` varchar(200) DEFAULT '' COMMENT '预留字段3',\n"
-			+ "  `created_by` varchar(32) NOT NULL DEFAULT 'auto' COMMENT '创建人',\n"
+	private final static String sqlString = "  `id` bigint(11) NOT NULL AUTO_INCREMENT,\n"
+			+ "  `member_id` varchar(32) COLLATE utf8mb4_bin NOT NULL COMMENT '商户id',\n"
+			+ "  `channel_member_id` varchar(32) COLLATE utf8mb4_bin NOT NULL COMMENT '商户渠道方id',\n"
+			+ "  `rsa_public_key` text COLLATE utf8mb4_bin COMMENT '公钥',\n"
+			+ "  `rsa_private_key` text COLLATE utf8mb4_bin COMMENT '私钥',\n"
+			+ "  `channel_id` int(16) NOT NULL COMMENT '渠道号:1 畅游',\n"
+			+ "  `counter_id` int(16) DEFAULT NULL COMMENT '柜台号',\n"
+			+ "  `options` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '转换积分渠道',\n"
+			+ "  `remark` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,\n"
+			+ "  `rem1` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,\n"
+			+ "  `rem2` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,\n"
+			+ "  `rem3` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,\n"
 			+ "  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',\n"
-			+ "  `updated_by` varchar(32) DEFAULT NULL COMMENT '最后更新人',\n"
 			+ "  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',";
 
 	private static String dtoName = "";
